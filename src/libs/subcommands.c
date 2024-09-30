3163ea944894a303adb7f0fa8ba1de20e6ef2217
Juan Burda <jburda@itba.edu.ar>
2024-09-30 16:37:32 -0300

    feat: embed subcommand

--- src/libs/subcommands.c
+++ src/libs/subcommands.c
@@ -5,6 +5,9 @@
 #include "../../include/subcommands.h"
 #include "../../include/constants/cli-arguments.h"
 #include "../include/bmp-utils.h"
+#include "../include/lsb1.h"
+#include "../include/bmp-utils.h"
+#include "../include/file-utils.h"
 
 typedef void (*SubcommandFunction)(ArgParser *parser);
 
@@ -41,11 +44,35 @@ void run_subcommand(ArgParser *parser) {
 }
 
 void embed_subcommand(ArgParser *parser) {
-    // TODO
-    printf("Ejecutando subcomando embed");
+    Argument* bmpFilePathArg = find_argument(parser->current_subcommand, ARG_CARRIER);
+    Argument* payloadFilePathArg = find_argument(parser->current_subcommand, ARG_PAYLOAD);
+
+    Argument* bmpFilePathOutputArg = find_argument(parser->current_subcommand, ARG_OUTPUT);
+
+    const unsigned char *bmpFilePath =  (unsigned char*)bmpFilePathArg->value;
+    const unsigned char *payloadFilePath = (unsigned char*)payloadFilePathArg->value;
+    const unsigned char *bmpFilePathOutput = (unsigned char*)bmpFilePathOutputArg->value;
+    const unsigned char *outputFileName = "resources/output";
+
+    BMPFileHeader fileHeader;
+    BMPInfoHeader infoHeader;
+
+    // Embed data
+    uint8_t* bmpData = read_bmp_data(bmpFilePath, &fileHeader, &infoHeader);
+    if (!bmpData)
+        exit(1);
+
+    const size_t payloadLength;
+    const char* payload = fmt_payload(payloadFilePath, &payloadLength);
+
+    lsb1(bmpData, infoHeader.biWidth, infoHeader.biHeight, infoHeader.biBitCount, payload, payloadLength);
+
+    write_bmp(bmpFilePathOutput, &fileHeader, &infoHeader, bmpData);
+
+    free(bmpData);
+    free(payload);
 }
 
 void extract_subcommand(ArgParser *parser) {
-    // TODO
-    printf("Ejecutando subcomando extract");
+
 }
--- src/main.c
+++ src/main.c
@@ -1,44 +1,16 @@
 #include <stdio.h>
-#include "../include/bmp-utils.h"
-#include "../include/file-utils.h"
 #include "../include/argument-parser.h"
 #include "../include/init-parser.h"
-#include "../include/lsb1.h"
-#include "../include/constants/cli-arguments.h"
+#include "../include/subcommands.h"
 
 int main(int argc, char *argv[]) {
     ArgParser *parser = init_steganography_parser();
 
     parse_arguments(parser, argc, argv);
+    
+    run_subcommand(parser);
 
-    Argument* bmpFilePathArg = find_argument(parser->current_subcommand, ARG_CARRIER);
-    Argument* payloadFilePathArg = find_argument(parser->current_subcommand, ARG_PAYLOAD);
-
-    Argument* bmpFilePathOutputArg = find_argument(parser->current_subcommand, ARG_OUTPUT);
-
-    const unsigned char *bmpFilePath =  (unsigned char*)bmpFilePathArg->value;
-    const unsigned char *payloadFilePath = (unsigned char*)payloadFilePathArg->value;
-    const unsigned char *bmpFilePathOutput = (unsigned char*)bmpFilePathOutputArg->value;
-    const unsigned char *outputFileName = "resources/output";
-
-    BMPFileHeader fileHeader;
-    BMPInfoHeader infoHeader;
-
-    // Embed data
-    uint8_t* bmpData = read_bmp_data(bmpFilePath, &fileHeader, &infoHeader);
-    if (!bmpData)
-        exit(1);
-
-    const size_t payloadLength;
-    const char* payload = fmt_payload(payloadFilePath, &payloadLength);
-
-    lsb1(bmpData, infoHeader.biWidth, infoHeader.biHeight, infoHeader.biBitCount, payload, payloadLength);
-
-    write_bmp(bmpFilePathOutput, &fileHeader, &infoHeader, bmpData);
-
-    free(bmpData);
-    free(payload);
-
+    /*
     // Extract data
     uint8_t* bmpSecretData = read_bmp_data(bmpFilePathOutput, &fileHeader, &infoHeader);
     if (!bmpSecretData) {
@@ -62,7 +34,8 @@ int main(int argc, char *argv[]) {
     free(bmpSecretData);
     free(extractedMessage);
 
-    free_parser(parser);
+    */
+   free_parser(parser);
 
     return 0;
 }
