45099bf0bcc0aaa341521d1b5be3bacf12caf4e0
Juan Burda <jburda@itba.edu.ar>
2024-11-03 16:16:19 -0300

    refactor: remove unused code in main

--- src/main.c
+++ src/main.c
@@ -9,9 +9,6 @@
 #include <stdio.h>
 #include <subcommands.h>
 
-// TODO: delete
-void example_encrypt_decrypt();
-
 int main(int argc, char *argv[]) {
     ArgParser *parser = init_steganography_parser();
 
@@ -23,64 +20,7 @@ int main(int argc, char *argv[]) {
 
     run_subcommand(parser);
 
-    /*
-    // Extract data
-    uint8_t* bmpSecretData = read_bmp_data(bmpFilePathOutput, &fileHeader, &infoHeader);
-    if (!bmpSecretData) {
-        exit(1);
-        char *extractedMessage = calloc(payloadLength, sizeof(char));
-    lsb1_extract(bmpSecretData, infoHeader.biWidth, infoHeader.biHeight, infoHeader.bi_bit_count, extractedMessage, payloadLength);
-    char content[payloadLength];
-    char extension[payloadLength];
-    size_t extractedMessageLength = dfmt_payload(extractedMessage, content, extension);
-    char *outputFilePathWithExtension = (char*)malloc(strlen((char*)outputFileName) + strlen((char*)extension) + 1);
-    sprintf(outputFilePathWithExtension, "%s%s", (char*)outputFileName, (char*)extension);
-    
-    create_file(outputFilePathWithExtension);
-    write_file(outputFilePathWithExtension, content, extractedMessageLength);
-    
-    free(outputFilePathWithExtension);
-    free(bmpSecretData);
-    free(extractedMessage);
-    */
     free_parser(parser);
 
     return SUCCESS;
-}
-
-void example_encrypt_decrypt() {
-    CipherParams *params = init_cipher_params("password", "aes256", "cbc");
-
-    const char *message = "Hello, World! This is a test message.";
-    uint8_t *plaintext = (uint8_t *)message;
-    int plaintext_len = strlen(message);
-
-    uint8_t *ciphertext = NULL;
-    int ciphertext_len = encrypt(params, plaintext, plaintext_len, &ciphertext);
-    if (ciphertext_len < 0) {
-        fprintf(stderr, "Encryption failed\n");
-        return;
-    }
-
-    printf("Original message: %s\n", message);
-    printf("Encrypted message (hex): ");
-    for (int i = 0; i < ciphertext_len; i++) {
-        printf("%02x", ciphertext[i]);
-    }
-    printf("\n");
-
-    uint8_t *decrypted_text = NULL;
-    int decrypted_len = decrypt(params, ciphertext, ciphertext_len, &decrypted_text);
-    if (decrypted_len < 0) {
-        fprintf(stderr, "Decryption failed\n");
-        free(ciphertext);
-        return;
-    }
-
-    // print decrypted message
-    printf("Decrypted message: %.*s\n", decrypted_len, decrypted_text);
-
-    free(ciphertext);
-    free(decrypted_text);
-    free_cipher_params(params);
 }
